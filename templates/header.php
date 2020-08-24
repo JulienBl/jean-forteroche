6b9d0ac4d0b558c120354d16052ec3bab724ac2b
JulienBl <julienblain@hotmail.com>
2020-08-24 14:25:40 +0200

    docotype duplicate

--- templates/header.php
+++ templates/header.php
@@ -1,20 +1,3 @@
-<!doctype html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8" />
-        <title>Titre du site</title>    
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="Blog écrivain" />
-       
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        
-        
-
-        
-    </head>
-
-    <body>
-
     <div id="image-header">
 
 
